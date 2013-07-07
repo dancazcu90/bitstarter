a81c1f70591eaa87d22e4c82731bfb775cdae875
dancazcu90 <dan.cazacu90@yahoo.com>
2013-07-07 10:27:06 +0000

    Update to print from index.html

--- web.js
+++ web.js
@@ -1,15 +1,9 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  response.send(fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-}));
-});
-
+  response.send(fs.readFileSync('index.html','utf-8'));
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
