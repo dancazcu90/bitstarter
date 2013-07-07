488bb8857d08ebd0530ef3661131d3d339a46cae
dancazcu90 <dan.cazacu90@yahoo.com>
2013-07-07 11:14:32 +0000

    Update to print from index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ app.get('/', function(request, response) {
   var fs = require('fs');
   //buf = new Buffer(256);
   buffer = fs.readFileSync('index.html','utf-8').toString();
-  response.send(buf.toString('utf8',0,len));
+  response.send(buffer);//buf.toString('utf8',0,len));
   //console.log(buffer);
 });
 var port = process.env.PORT || 5000;
