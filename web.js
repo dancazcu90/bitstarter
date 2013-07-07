6ea5a768e0482a134cc00cdcbe8a0067f97a25e9
dancazcu90 <dan.cazacu90@yahoo.com>
2013-07-07 11:13:10 +0000

    Update to print from index.html

--- web.js
+++ web.js
@@ -3,13 +3,13 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  buf = new Buffer(256);
-  len=buf.write(fs.readFileSync('index.html','utf-8'));
-  response.send(buf.toString('utf8'));
-  //console.log(buf.toString('utf8',0,len));
+  //buf = new Buffer(256);
+  buffer = fs.readFileSync('index.html','utf-8').toString();
+  response.send(buf.toString('utf8',0,len));
+  //console.log(buffer);
 });
- var port = process.env.PORT || 5000;
- app.listen(port, function() {
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
 	console.log("Listening on " + port);
- });
+});
 
