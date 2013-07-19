4b69150e7d29410c226a13bcf4d7a8fda79879f1
dancazcu90 <dan.cazacu90@yahoo.com>
2013-07-19 20:16:11 +0000

    Added style to index.html

--- web.js
+++ web.js
@@ -3,12 +3,10 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  //buf = new Buffer(256);
   buffer = fs.readFileSync('index.html','utf-8').toString();
-  response.send(buffer);//buf.toString('utf8',0,len));
-  //console.log(buffer);
+  response.send(buffer);
 });
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
